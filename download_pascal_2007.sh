1a13fdc267859efc94f7f997174f73a3e2def1ce
Merhawi <45622655+mtm-007@users.noreply.github.com>
2025-06-26 18:50:53 +0000

    modify the download_pascal_dataset script to use pytorch cpu

--- download_pascal_2007.sh
+++ download_pascal_2007.sh
@@ -1,6 +1,12 @@
 python -m venv /tmp/fastai-tmp
 source /tmp/fastai-tmp/bin/activate
+
+# Install CPU-only PyTorch first
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+# Then install fastai (which will use the already installed CPU PyTorch)
 pip install fastai
+
 python - <<EOF
 from fastai.vision.all import untar_data, URLs
 import shutil, os
@@ -10,6 +16,7 @@ save_dir = './data'
 
 os.makedirs(save_dir, exist_ok=True)
 shutil.copytree(path, os.path.join(save_dir, 'pascal_2007'), dirs_exist_ok=True)
+print("succefully downloaded data!")
 EOF
 deactivate
 rm -rf /tmp/fastai-tmp

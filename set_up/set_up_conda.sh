316215850bee8d283827a88697e6cd182add5651
Merhawi <merhawitsegay007@gmail.com>
2025-07-10 01:46:39 +0000

    add torch cpu based option

--- set_up/set_up_conda.sh
+++ set_up/set_up_conda.sh
@@ -9,9 +9,21 @@ conda activate older_pytorch
 pip install --upgrade pip
 pip install uv
 
+# 3.5. Install conda-only packages
+conda install -c conda-forge cudatoolkit=9.0 -y
+conda install -c conda-forge bcolz=1.2.0 -y
+conda install -c conda-forge xorg-libsm -y
+conda install -c conda-forge xorg-libxext -y
+conda install -c conda-forge xorg-libxrender -y
+
 # 3. Install torch and torchvision from URLs (must be first)
 uv pip install https://download.pytorch.org/whl/cu90/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
 uv pip install torchvision==0.2.1 -f https://download.pytorch.org/whl/cu90/torch_stable.html
+
+# For CPU only:
+# uv pip install https://download.pytorch.org/whl/cpu/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
+# uv pip install torchvision==0.2.1 -f https://download.pytorch.org/whl/cpu/torch_stable.html
+
 
 # 4. Install remaining dependencies from pyproject.toml
 uv sync

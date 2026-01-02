62c54901fc4c91b17ebbb37458f83f94de8bcf80
Merhawi <merhawitsegay007@gmail.com>
2026-01-02 07:52:36 +0000

    modify dataset downloader script

--- download_datasets_from_fastai.sh
+++ download_datasets_from_fastai.sh
@@ -11,11 +11,11 @@ python - <<EOF
 from fastai.vision.all import untar_data, URLs
 import shutil, os
 
-path = untar_data(URLs.PASCAL_2007)
+path = untar_data(URLs.cifar10)
 save_dir = './data'
 
 os.makedirs(save_dir, exist_ok=True)
-shutil.copytree(path, os.path.join(save_dir, 'pascal_2007'), dirs_exist_ok=True)
+shutil.copytree(path, os.path.join(save_dir, 'cifar10'), dirs_exist_ok=True)
 print("succefully downloaded data!")
 EOF
 deactivate
